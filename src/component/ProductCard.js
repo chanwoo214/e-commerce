37317cd4cf7b0f684375bd141fb868cc842895ef
Chan Woo SHIM <chanwooshim@Chans-MacBook-Air.local>
2024-08-22 14:25:03 +1200

    lesson 5

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src= "https://lp2.hm.com/hmgoepprod?set=source[/fd/50/fd505c07abb9dfb31419419dd9f9d1abda66b31e.jpg],origin[dam],category[men_jacketscoats_jackets],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]"/>
+        <div>conscious choice</div>
+        <div>Linden Emb Shirt</div>
+        <div>$49.99</div>
+        <div>New Arrival</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState(null);
+    const getProducts = async () => {
+        let url = `http://localhost:4000/products`
+        let response = await fetch(url);
+        let data = await response.json();
+        setProductList(data);
+    }
+
+    useEffect (() => {
+
+        getProducts();
+
+    },[])
   return (
-    <div>Product All Page</div>
+    <div>
+        <ProductCard/>
+    </div>
   )
 }
 
